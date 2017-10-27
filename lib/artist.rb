153d0ff65dcf3d1e566a2c19a58f333262dda97f
Monica  Karasek <mlkarasek@gmail.com>
2017-10-27 05:47:20 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+class Artist 
+
+attr_accessor :name
+
+  def initialize(name)
+    @name = name 
+  end 
+
+  def songs 
+    @songs 
+  end 
+
+  def find_or_create_by_name
+  end 
+
+  def print_songs
+  end 
+
+  
+
+end 
